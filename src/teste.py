dfae68df0a8522b6669f883fac2fdcaeebbcd72c
Guilherme-Bodart <guilhermebodart73@gmail.com>
2019-06-24 22:44:46 -0300

    Trabalho Final
    Começando o trabalho

---
+++ src/teste.py
@@ -0,0 +1,75 @@
+from lark import Lark
+grammar = """
+    start: sttmt+
+
+    sttmt : (expr) ";"
+
+    expr : (assign) 
+            | (ifexpr)
+            | (whileexpr)
+            | (blockexpr)
+            | (lexpr) 
+            | (defun)
+
+    defun : "def" (ident) "(" paramlist? ")" (expr)
+
+    paramlist : (ident) ("," (ident))*
+    
+    ifexpr : "if" (expr) "then" (expr) ("else" (expr))?
+            
+    whileexpr : "while" (expr) "do" (expr)
+
+    blockexpr : "{" (sttmt) "}"
+
+    assign : "var"? (ident) "=" (expr)        
+
+    lexpr : (disj) ("or" (disj))*
+
+    disj : (conj) ("and" (conj))*
+
+    conj : (aexpr) ((rel) (aexpr))?
+
+    rel : "==" | "!=" | ">" | ">=" | "<" | "<="
+
+    aexpr : (factor) (("+"|"-") (factor))*
+
+    factor : (term) (("*"|"/") (term))*
+
+    term : (atom) ("^" (atom))?
+
+    atom : "-" (atom)
+                | "not" (atom)
+                | "(" (expr) ")"
+                | NUMBER
+                | (functioncall)
+                | (variable)   
+
+    functioncall : (ident) "(" (arglist)? ")"
+
+    arglist : (expr) ("," (expr))*
+
+    variable : (ident)
+
+    ident : (CHAR+)(CHAR|NUMBER)*
+
+    CHAR: LETTER+  
+    %import common.NUMBER
+    %import common.LETTER
+    %import common.WS_INLINE
+    %ignore WS_INLINE
+
+"""
+   
+
+parser = Lark(grammar)
+
+
+def main():    
+    expression = input("\nEscreve uma expressao: ")
+    while True:
+        print(parser.parse(expression))    
+        expression = input("\nEscreve uma expressao ou aberte enter: ")
+        if expression == "":
+            break 
+if __name__ == '__main__':
+    main()
